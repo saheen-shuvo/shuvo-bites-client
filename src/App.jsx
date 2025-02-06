1dc8e19a6ca8185636e9cddf5cc9e5b51935d364
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-02-06 22:41:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-red-600'>Vite + React</h1>
+      <button className='btn btn-primary'>Hi</button>
+    </>
+  )
+}
+
+export default App
